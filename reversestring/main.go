aa4ea861f39aadbaba5ac77a5ba076072576791a
BaazaouiHamza <hamza.baazaoui@esprit.tn>
2023-06-12 13:59:59 +0100

    string reversal problem

---
+++ reversestring/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "strings"
+
+func reverseString(str string) string {
+	arr := strings.Split(str, "")
+	rev := make([]string, 0)
+
+	for i := len(arr) - 1; i >= 0; i-- {
+		rev = append(rev, arr[i])
+	}
+	return strings.Join(rev, "")
+}
+
+func main() {
+	println(reverseString("gogogogo"))
+}
